0341169ce225083adf081a8eeb0b2d36e26ae3ca
mikiteek <mikiteek@gmail.com>
2020-09-20 16:02:42 +0300

    Updates

--- precache-manifest.3183d454d8ca972804363a42c1bd8de5.js
+++ precache-manifest.3183d454d8ca972804363a42c1bd8de5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e7c3850adf7cdcb16a3843c188e72084",
+    "revision": "009a193df3b94ca01c778d308fee250e",
     "url": "/github.io/goit-react-hw-01-components/index.html"
   },
   {
-    "revision": "2868ea6fed2ba3905f54",
+    "revision": "bbfcfd4fb7838418d462",
     "url": "/github.io/goit-react-hw-01-components/static/css/main.c282d304.chunk.css"
   },
   {
-    "revision": "82c09a271dc74205808e",
-    "url": "/github.io/goit-react-hw-01-components/static/js/2.729d2d4c.chunk.js"
+    "revision": "8702e87f2de9d9d43916",
+    "url": "/github.io/goit-react-hw-01-components/static/js/2.f39d56b2.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/github.io/goit-react-hw-01-components/static/js/2.729d2d4c.chunk.js.LICENSE.txt"
+    "url": "/github.io/goit-react-hw-01-components/static/js/2.f39d56b2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2868ea6fed2ba3905f54",
-    "url": "/github.io/goit-react-hw-01-components/static/js/main.e265392a.chunk.js"
+    "revision": "bbfcfd4fb7838418d462",
+    "url": "/github.io/goit-react-hw-01-components/static/js/main.1bae7ada.chunk.js"
   },
   {
     "revision": "d543f3f7f76a2ad070f8",
