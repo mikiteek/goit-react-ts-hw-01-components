a382a1318a67899391c97267b44e835565263452
mikiteek <mikiteek@gmail.com>
2020-09-22 14:28:09 +0300

    Updates

---
+++ precache-manifest.3183d454d8ca972804363a42c1bd8de5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "009a193df3b94ca01c778d308fee250e",
+    "url": "/github.io/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "bbfcfd4fb7838418d462",
+    "url": "/github.io/goit-react-hw-01-components/static/css/main.c282d304.chunk.css"
+  },
+  {
+    "revision": "8702e87f2de9d9d43916",
+    "url": "/github.io/goit-react-hw-01-components/static/js/2.f39d56b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/goit-react-hw-01-components/static/js/2.f39d56b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbfcfd4fb7838418d462",
+    "url": "/github.io/goit-react-hw-01-components/static/js/main.1bae7ada.chunk.js"
+  },
+  {
+    "revision": "d543f3f7f76a2ad070f8",
+    "url": "/github.io/goit-react-hw-01-components/static/js/runtime-main.57b42ba3.js"
+  }
+]);
